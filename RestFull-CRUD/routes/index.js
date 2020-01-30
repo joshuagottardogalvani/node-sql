0401d92ae4803dca5193b98c504629c031cf42ba
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2020-01-30 13:16:53 +0000

    6.3

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -7,32 +7,13 @@ const config = {
   server: "213.140.22.237",
   /* database: 'School' */
 }
+const path = require('path');
+const http = require('http');
+const app= express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/unita', function(req, res, next) {
-  sql.connect(config, err => {
-    if(err) console.log(err);
-    let sqlRequest = new sql.Request();
-    sqlRequest.query('SELECT * FROM [cr-unit-attributes]', (err, result) => {
-        if (err) console.log(err);
-        res.send(result);
-    });
-  });
-});
-
-router.get('/unita/:nome', function(req, res, next) {
-  sql.connect(config, err => {
-    if(err) console.log(err);
-    let sqlRequest = new sql.Request();
-    sqlRequest.query(`SELECT * FROM [cr-unit-attributes] WHERE Unit = '${req.params.nome}'`, (err, result) => {
-        if (err) console.log(err);
-        res.send(result);
-    });
-  });
 });
 
 module.exports = router;
--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -1,33 +1,50 @@
 var express = require('express');
 var router = express.Router();
-const sql = require('mssql')
+const sql = require('mssql');
+var createError = require('http-errors');
 const config = {
-  user: 'gottardo.joshua',
+  user: 'bellati.samuele',
   password: 'xxx123#',
   server: "213.140.22.237",
   /* database: 'School' */
-}
+};
 
-router.get('/attori', function(req, res, next) {
+router.get('/unita', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);
     let sqlRequest = new sql.Request();
-    sqlRequest.query('SELECT * FROM [School].[Person]', (err, result) => {
+    sqlRequest.query('SELECT * FROM [cr-unit-attributes]', (err, result) => {
         if (err) console.log(err);
         res.send(result);
     });
   });
 });
 
-router.get('/attori/:name', function(req, res, next) {
+router.get('/unita/:nome', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);
     let sqlRequest = new sql.Request();
-    sqlRequest.query(`SELECT * FROM [School].[Person] where FirstName = '${req.params.name}'`, (err, result) => {
+    sqlRequest.query(`SELECT * FROM [cr-unit-attributes] WHERE Unit = '${req.params.nome}'`, (err, result) => {
         if (err) console.log(err);
         res.send(result);
     });
   });
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  let unit = req.body;
+  if (!unit) {
+    next(createError(400 , "Please provide a correct unit"));
+  }
+  sql.connect(config, err => {
+    let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit, Cost, Hit_Speed, Speed) VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}','${unit.Speed}')`;
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(sqlInsert, (error, results) => {
+      if (error) throw error;
+      return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+    });
+  })
 });
 
 module.exports = router;
